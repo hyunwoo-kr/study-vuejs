0f6cbca1510cfce631b1e4d31c7ea500b4cf6bb8
hyunwoo <khw88309@finotek.co.kr>
2022-08-23 15:53:07 +0900

    컴포넌트 등록

--- 7_todo/src/App.vue
+++ 7_todo/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-<div id="app"></div>
+<div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+</div>
 </template>
 
 <script>
-export default {}
+import TodoHeader from './components/TodoHeader'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+import TodoFooter from './components/TodoFooter'
+
+export default {
+    components: {
+        'TodoHeader': TodoHeader,
+        'TodoInput': TodoInput,
+        'TodoList': TodoList,
+        'TodoFooter': TodoFooter
+    }
+}
 </script>
 
 <style>
