d46a312d9178b23e9eb9e563a92bb9daf77f8867
hyunwoo <khw8309@gmail.com>
2022-08-27 20:32:07 +0900

    vue plugin 등록 - global function 추가

--- 8_todo_vuex/src/main.js
+++ 8_todo_vuex/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import common from './assets/js/common.js'
 import store from './store' // index.js 라면 생략 가능
 
+Vue.use(common);
+
 new Vue({
-    store, // #vuex 넣기
-    el: '#app',
-    render: h => h(App)
+	store, // #vuex 넣기
+	el: '#app',
+	render: h => h(App)
 })
